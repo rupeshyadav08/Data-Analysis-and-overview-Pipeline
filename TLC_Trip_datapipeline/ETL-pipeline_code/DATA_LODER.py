57f3b145fd8efbee6fa0ae7e72d4fa22d8b534eb
rupeshyadav08 <47237661+rupeshyadav08@users.noreply.github.com>
2023-08-21 13:56:38 +0530

    Add files via upload

---
+++ TLC_Trip_datapipeline/ETL-pipeline_code/DATA_LODER.py
@@ -0,0 +1,27 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = 'https://storage.googleapis.com/uber_data_pipeline/Data/yellow_tripdata.parquet'
+    response = requests.get(url)
+
+    Data = pd.read_parquet(url, engine='pyarrow')
+    return pd.DataFrame(Data[:100000])
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
